5e6545a3bf223481b086e980b81ebeed80b322ef
Mikol <mikokondonikolau@interia.pl>
2025-10-31 17:32:57 +0100

    Fix internet speed calculation and add conversion

--- what.js
+++ what.js
@@ -1,4 +1,5 @@
 var iSpeed = Number(prompt("What is your Internet speed in Mb/s, Use https://speedtest.net");
 var hour = 3600
-var iSpeed2 = (iSpeed * hour) / 1024
+var iSpeed2 = iSpeed / 8
+var iSpeed3 = (iSpeed2 * hour) / 1024
 alert(iSpeed2)
